3cb1e855dbde03e7200ddea86dbdb861ce6e14f6
Vitamax Oduol <85936735+vitamaxoduol@users.noreply.github.com>
2023-06-14 20:02:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
+
+
+
+const random = Math.floor(Math.random() * (Number.MAX_SAFE_INTEGER - 1)) + 1;
+console.log(random);
+
+const num3 = 5
+const num4 = 24
+const mod = num4 % num3;
+console.log(mod);
+
+
+const setNum = [5, 8, 15, 20, 18];
+const max = Math.max(5, 8, 15, 20, 18);
+console.log(max);
